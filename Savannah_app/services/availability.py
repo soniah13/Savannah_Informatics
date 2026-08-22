0803a0c41db846dc9f48e82378aec5765699fe84
sonia <imorgansonia@gmail.com>
2026-08-22 16:55:43 +0300

    feat: add availability ifile and functions to check if the availability of the doctor depending on their working hours, if they are eligible and any conflicting appointment

---
+++ Savannah_app/services/availability.py
@@ -0,0 +1,42 @@
+from datetime import timedelta, datetime
+from django.utils import timezone
+from models import *
+
+
+Appointment_Duration = timedelta(minutes=30)
+Booking_Notice = timedelta(hours=1)
+
+
+
+# This function check if the doctor is available/scheduled to work during that precified slot
+# It returns a boolean value (True/False)
+def Doctor_is_available_slot(doctor:Doctor, appointment_date, appointment_time):
+    weekday = appointment_date.weekday()
+    slot_start = appointment_time
+    slot_end = (datetime.combine(appointment_date, appointment_time) + Appointment_Duration).time()
+
+    return DoctorWorkingHours.objects.filter(doctor=doctor,
+                                             weekday=weekday,
+                                             start_time_lte = slot_start,
+                                             end_time_gte = slot_end,).exists()
+
+
+# Since every appointment is 30min, matching the same doctor/date and time is enough to use to detect a conflict
+def conflicting_appointment(doctor:Doctor, appointment_date, appointment_time):
+    return Appointment.objects.filter(doctor=doctor,
+                                      appointment_date=appointment_date,
+                                      appointment_time=appointment_time).exists()
+
+
+# returns doctors whocan serve the requested slot depending on their speciality and service requested
+# by checking if doctor is active, has required speciality, works during the requested slot and has no existing appointment in the slot
+def get_eligible_doctors(clinical_service:ClinicalService, appointment_date, appointment_time):
+    doctors = (Doctor.objects.filter(Specialities=clinical_service.speciality, is_active=True,).distinct())
+    eligible_doctors = []
+    for doctor in doctors:
+        if not Doctor_is_available_slot(doctor, appointment_date, appointment_time):
+            continue
+        if conflicting_appointment(doctor, appointment_date, appointment_time):
+            continue
+        eligible_doctors.append(doctor)
+    return eligible_doctors
