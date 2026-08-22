6cf37082fbcae4c65aaf3b8c164a29adfa37de3a
sonia <imorgansonia@gmail.com>
2026-08-22 07:21:19 +0300

    feat: setup database using postgreql and secure credentials

--- Savannah_project/settings.py
+++ Savannah_project/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Savannah_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgreql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'PORT': os.getenv('DB_PORT'),
+        'HOST': os.getenv('DB_HOST'),
     }
 }
 
