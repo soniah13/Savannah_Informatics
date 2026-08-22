b3e3c3b537adf1cb2b57522359acc3c7789ec08b
sonia <imorgansonia@gmail.com>
2026-08-22 12:20:27 +0300

    feat: link projects url to apps url and link.

--- Savannah_project/urls.py
+++ Savannah_project/urls.py
@@ -20,7 +20,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/',include('savannah_app.urls')),
+    path('api/v1/',include('Savannah_app.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
