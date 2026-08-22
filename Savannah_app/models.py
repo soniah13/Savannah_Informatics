d849a6433f4da558686dcbbd03a47d204df69d8e
sonia <imorgansonia@gmail.com>
2026-08-22 08:16:08 +0300

    feat: add doctor,speciality, clinical services and doctor working hours models and register to admin.py

--- Savannah_app/models.py
+++ Savannah_app/models.py
@@ -13,6 +13,44 @@ class Patient(models.Model):
     def __str__(self):
         return self.full_name()
 
+class Speciality(models.Model):
+    speciality_name=models.CharField(max_length=100)
+    speciality_description=models.TextField(blank=True)
+
+    def __str__(self):
+            return self.speciality_name()
+
+class ClinicalService(models.Model):
+    service_name=models.CharField(max_length=150)
+    speciality=models.ForeignKey(Speciality,on_delete=models.PROTECT, related_name="clinical_services")
+    service_description=models.TextField(blank=True)
+
+    def __str__(self):
+            return self.service_name()
+
+class Doctor(models.Model):
+    full_name=models.CharField(max_length=200)
+    email=models.EmailField()
+    phone_number=models.CharField(max_length=20)
+    Specialities=models.ManyToManyField(Speciality, related_name='doctors')
+    is_active=models.BooleanField(default=True)
+
+    def __str__(self):
+            return self.full_name()
+
+class DoctorWorkingHours(models.Model):
+    class Weekday(models.IntegerChoices):
+        MONDAY=0,'Monday'
+        TUESDAY=1,'Tuesday'
+        WEDNESDAY=2, 'Wednesday'
+        THURSDAY=3, 'Thursday'
+        FRIDAY=4, 'Friday'
+        SATURDAY=5, 'Saturday'
+        SUNDAY=6, 'Sunday'
+    doctor = models.ForeignKey(Doctor,on_delete=models.CASCADE,related_name='working_hours')
+    weekday= models.PositiveSmallIntegerField(choices=Weekday.choices)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
 
 
 
